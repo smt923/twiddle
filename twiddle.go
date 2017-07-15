1aaef299131c53b0a83ee41ee33c405608f96498
smt923 <smtea923@gmail.com>
2017-07-15 16:47:44 +0100

    these don't need len check

--- twiddle.go
+++ twiddle.go
@@ -28,20 +28,20 @@ func twiddle(t string, extra bool, light bool) {
 		fmt.Println(t + "0")
 		fmt.Println(t + "1")
 		if extra {
+			fmt.Println(strings.ToUpper(t))
+			fmt.Println(strings.ToUpper(string(t[0])) + t[1:])
+			fmt.Println(strings.ToUpper(string(t[0])) + t)
+			fmt.Println(string(t[len(t)-1]) + t[:len(t)-1])
+			fmt.Println(string(t[len(t)-1]) + string(t[len(t)-1]) + t)
+			fmt.Println(string(t[len(t)-1]) + string(t[len(t)-1]) + t[:len(t)-1])
+			fmt.Println(string(t[len(t)-1]) + t + "0")
+			fmt.Println(string(t[len(t)-1]) + t + "1")
 			if len(t) > 1 {
-				fmt.Println(strings.ToUpper(t))
-				fmt.Println(strings.ToUpper(string(t[0])) + t[1:])
-				fmt.Println(strings.ToUpper(string(t[0])) + t)
 				fmt.Println(t[:2] + t)
-				fmt.Println(string(t[len(t)-1]) + t[:len(t)-1])
-				fmt.Println(string(t[len(t)-1]) + string(t[len(t)-1]) + t)
-				fmt.Println(string(t[len(t)-1]) + string(t[len(t)-1]) + t[:len(t)-1])
-				fmt.Println(string(t[len(t)-1]) + t + "0")
-				fmt.Println(string(t[len(t)-1]) + t + "1")
 			}
 		}
 	}
 	fmt.Println(t)
-	fmt.Println(string(t[0]) + t)
-	fmt.Println(string(t[len(t)-1]) + t)
+	fmt.Println(string(t[0]) + t)        // aliceh -> aaliceh
+	fmt.Println(string(t[len(t)-1]) + t) // aliceh -> haliceh
 }
