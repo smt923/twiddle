b1c968e8b545e69d97e9845a3db4e6454dbca0a2
smt923 <smtea923@gmail.com>
2017-07-15 16:33:09 +0100

    add src file

---
+++ twiddle.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	extraFlag := flag.Bool("e", false, "extra: Should we add some extra string fiddling?")
+	lightFlag := flag.Bool("l", false, "light: Print just a few common substitutions (overrides -e)")
+	flag.Parse()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if len(text) > 0 {
+			twiddle(text, *extraFlag, *lightFlag)
+		}
+	}
+}
+
+func twiddle(t string, extra bool, light bool) {
+	if !light {
+		fmt.Println(t + t)
+		fmt.Println(t + "0")
+		fmt.Println(t + "1")
+		if extra {
+			if len(t) > 1 {
+				fmt.Println(strings.ToUpper(t))
+				fmt.Println(strings.ToUpper(string(t[0])) + t[1:])
+				fmt.Println(strings.ToUpper(string(t[0])) + t)
+				fmt.Println(t[:2] + t)
+				fmt.Println(string(t[len(t)-1]) + t[:len(t)-1])
+				fmt.Println(string(t[len(t)-1]) + string(t[len(t)-1]) + t)
+				fmt.Println(string(t[len(t)-1]) + string(t[len(t)-1]) + t[:len(t)-1])
+				fmt.Println(string(t[len(t)-1]) + t + "0")
+				fmt.Println(string(t[len(t)-1]) + t + "1")
+			}
+		}
+	}
+	fmt.Println(string(t[0]) + t)
+	fmt.Println(string(t[len(t)-1]) + t)
+}
